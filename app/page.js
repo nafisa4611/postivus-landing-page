a399a20735b265dae3ae5d105b54f57fc083b61d
nafisa4611 <jabinmila@gmail.com>
2026-01-27 21:31:26 +0600

    navbar created

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-16">
       <NavBar />
       <h1>Welcome to the Home Page</h1>
     </div>
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,16 +1,28 @@
 import Image from "next/image";
-import Logo from "../public/logo/logo-white-2.svg";
+import Logo from "../public/logo/logo-dark-2.svg";
+import Link from "next/link";
+import { Button } from "@/components/ui/button"
 
 
 export default function NavBar() {
   return (
-    <div>
-        <Image
+    <div className="flex justify-between items-center">
+        <div className="">
+          <Image
           src={Logo}
           alt="Logo"
-          width={100}
+          width={350}
           height={100}
         />
+        </div>
+        <div className="text-3xl space-x-12 px-10 flex items-center"> 
+          <Link href="/about" className="hover:text-[#b9ff66]">About</Link>
+          <Link href="/services" className="hover:text-[#b9ff66]">Services</Link>
+          <Link href="/use-cases" className="hover:text-[#b9ff66]">Use Cases</Link>
+          <Link href="/pricing" className="hover:text-[#b9ff66]">Pricing</Link>
+          <Link href="/blog" className="hover:text-[#b9ff66]">Blog</Link>
+          <Button className="w-56 h-20 px-20 border-2 text-2xl hover:bg-[#b9ff66] cursor-pointer">Request a Quote</Button>
+        </div>
     </div>
   )
 }
