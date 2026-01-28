dfe4dbcce6bc69d16239ad8a7e742ce4c31b8694
nafisa4611 <jabinmila@gmail.com>
2026-01-28 13:04:34 +0600

    updated header

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <div className="flex flex-col md:flex-row justify-between items-center py-10 bg-bg transition-colors duration-300">
       <div className="">
-        <h1 className="font-medium text-6xl leading-tight">
+        <h1 className="font-medium text-6xl md:tex-5xl leading-tight">
           Navigating the <br/>digital landscape <br/>  for success
         </h1>
         <p className="font-normal text-2xl mt-6 leading-relaxed">
@@ -16,7 +16,7 @@ export default function Header() {
         </p>
         <Button className="w-56 h-20 px-20 mt-10 border-2 text-xl bg-black hover:bg-[#b9ff66] cursor-pointer text-white rounded-2xl ">Book a Consultation</Button>
       </div>
-      <div className="mr-6">
+      <div className="mr-6 border">
         <Image src={HeroHome} alt="Hero Home" width={800} height={480}/>
       </div>
     </div>
